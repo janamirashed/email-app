af7fc114300fbb082a06829dbca68a73c1b93a9d
nelatawy <nour.atawy2015@gmail.com>
2025-12-03 14:12:11 +0200

    swapped setters and getters with lombok's

--- backend/src/main/java/com/example/backend/model/AttachmentFS.java
+++ backend/src/main/java/com/example/backend/model/AttachmentFS.java
@@ -1,5 +1,10 @@
 package com.example.backend.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AttachmentFS {
 
     private String attachment_id;
@@ -8,14 +13,6 @@ public class AttachmentFS {
         this.attachment_id = attachment_id;
     }
     public AttachmentFS() {}
-
-    public String getAttachment_id() {
-        return attachment_id;
-    }
-
-    public void setAttachment_id(String attachment_id) {
-        this.attachment_id = attachment_id;
-    }
 
     @Override
     public String toString() {
--- backend/src/main/java/com/example/backend/model/EmailFS.java
+++ backend/src/main/java/com/example/backend/model/EmailFS.java
@@ -1,10 +1,14 @@
 package com.example.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Getter
+@Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class EmailFS {
     private String message_id;
@@ -26,62 +30,6 @@ public class EmailFS {
     }
 
     public EmailFS() {}
-
-    public String getMessage_id() {
-        return message_id;
-    }
-
-    public void setMessage_id(String message_id) {
-        this.message_id = message_id;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public List<String> getAttachments() {
-        return attachments;
-    }
-
-    public void setAttachments(List<String> attachments) {
-        this.attachments = attachments;
-    }
 
     @Override
     public String toString() {
